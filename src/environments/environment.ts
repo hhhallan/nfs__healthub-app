94a12437c4c783b63d0f513a8a7b2171dff5549e
ClemBlue <clement.blin76@gmail.com>
2022-03-10 17:14:47 +0100

    ajout route

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,14 @@
 export const environment = {
   production: false,
   API: {
-    API_CAT: 'http://localhost:3000/categorie/',
-    API_ACH: 'http://localhost:3000/achivement/',
-    API_USER: 'http://localhost:3000/user/'
+    API_CAT: 'http://127.0.0.1:8000/api/categories/',
+    API_ACH: 'http://127.0.0.1:8000/api/achievements/',
+    API_USER: 'http://127.0.0.1:8000/api/users/',
+    API_MET: 'http://127.0.0.1:8000/api/metrixes/',
+    API_USER_ACH: 'http://127.0.0.1:8000/api/user_achievements/',
+    API_USER_CAT: 'http://127.0.0.1:8000/api/user_categories/',
+    API_USER_MET: 'http://127.0.0.1:8000/api/user_metrixes/',
+    API_CAT_ON: 'http://127.0.0.1:8000/category_unable/',
+    API_CAT_OFF: 'http://127.0.0.1:8000/category_disable/'
   }
 };
